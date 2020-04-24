8fd076c2b4c094d9edbf0e4fe1a08667a080d73b
katriel <katriel181920@gmail.com>
2020-04-23 19:32:15 -0500

    padre interactúa con el niño a través del  variable local

--- src/app/countdown-parent/countdown-parent.component.ts
+++ src/app/countdown-parent/countdown-parent.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-countdown-parent',
-  templateUrl: './countdown-parent.component.html',
+  template: //la variables local es 'tiempo' que hace uso del hijo 'countdown-timer'
+  `
+  <h3>Countdown to Liftoff (via local variable)</h3>
+  <button (click)="tiempo.start()">Start</button>
+  <button (click)="tiempo.stop()">Stop</button>
+  <div class="seconds">{{tiempo.seconds}}</div>
+  <app-countdown-timer #tiempo></app-countdown-timer>
+  `,
   styleUrls: ['./countdown-parent.component.css']
 })
 export class CountdownParentComponent implements OnInit {
--- src/app/countdown-timer/countdown-timer.component.ts
+++ src/app/countdown-timer/countdown-timer.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy,  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-countdown-timer',
-  templateUrl: './countdown-timer.component.html',
+  template:'<p>{{message}}</p>',
   styleUrls: ['./countdown-timer.component.css']
 })
-export class CountdownTimerComponent implements OnInit {
+export class CountdownTimerComponent implements OnInit , OnDestroy{
 
   constructor() { }
 
-  ngOnInit(): void {
+  /*ngOnInit(): void {
+  }*/
+
+  intervalId = 0;
+  message = '';
+  seconds = 11;
+
+  clearTimer() { clearInterval(this.intervalId); }
+
+  ngOnInit()    { this.start(); }//inicializa
+  ngOnDestroy() { this.clearTimer(); }//limpia
+
+  start() { this.countDown(); }
+  stop()  {
+    this.clearTimer();
+    this.message = `Holding at T-${this.seconds} seconds`;
+  }
+
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.seconds -= 1;
+      if (this.seconds === 0) {
+        this.message = 'Blast off!';
+      } else {
+        if (this.seconds < 0) { this.seconds = 10; } // reset
+        this.message = `T-${this.seconds} seconds and counting`;
+      }
+    }, 1000);
   }
 
 }
